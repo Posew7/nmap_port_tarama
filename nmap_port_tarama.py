ca24adf06c1745f657dcc1bb0576ed5b8378393f
posew7 <posew7@gmail.com>
2019-04-14 13:43:12 +0100

    nmap ile port tarama

--- nmap_port_tarama.py
+++ nmap_port_tarama.py
@@ -1,7 +1,15 @@
 import os
 
-def isIp(ip_adresi):
+def isIp(kelime):
 
+    global flag
+    flag = False
+    for i in kelime:
+        if (i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9" or i == "0" or i == "."):
+            flag = True
+        else:
+            flag = False
+            break
 
 os.system("apt-get install nmap")
 os.system("apt-get install figlet")
@@ -23,15 +31,22 @@ islem = int(input("Yapmak İstediğiniz İşlemi Girin\n\n"))
 
 if (islem == 1):
     ip_adresi = input("\nIp Adresini Girin\n\n")
-    os.system("nmap " + ip_adresi)
+    isIp(ip_adresi)
+    if (flag):
+        os.system("nmap " + ip_adresi)
+    else:
+        print("olmadı")
 elif (islem == 2):
     ip_adresi = input("\nIp Adresini Girin\n\n")
+    isIp(ip_adresi)
     os.system("nmap -p- -sV " + ip_adresi)
 elif (islem == 3):
     ip_adresi = input("\nIp Adresini Girin\n\n")
+    isIp(ip_adresi)
     os.system("nmap -0 " + ip_adresi)
 elif (islem == 4):
     ip_adresi = input("\nIp Adresini Girin\n\n")
+    isIp(ip_adresi)
     os.system("nmap -p- -sV -A " + ip_adresi)
 else:
     print("Yanlış Değer Girdiniz")
